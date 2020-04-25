4889a375ee72e429eae00b0f07581d7251ab7485
PavanKumar-98 <63774781+PavanKumar-98@users.noreply.github.com>
2020-04-25 20:28:49 +0530

    Add files via upload

---
+++ assignment-2.py
@@ -0,0 +1,10 @@
+
+l = int(input("enter the dimensions l\n"))
+m = int(input("enter the dimensions m\n"))
+n = int(input("enter the dimensions n\n"))
+i=0
+
+while i < l:
+	mat = [["*"]*m]*n
+	print(mat)
+	i = i+1
