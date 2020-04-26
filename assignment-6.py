f9c89dd894d828ec1021f48b592edc9a25178cd8
PavanKumar-98 <63774781+PavanKumar-98@users.noreply.github.com>
2020-04-26 17:22:23 +0530

    Add files via upload

---
+++ assignment-6.py
@@ -0,0 +1,37 @@
+database = {}
+database ={"aaa":[20,30,30],"bbb":[33,55,66]}
+Math = 0
+Physics = 1
+Chemistry = 2
+Biology = 3
+Social-Science =4
+x=0
+name = input("enter the name ")
+subject = input("enter the subject")
+if subject == "math":
+	x = 0
+	
+elif (subject == "Physics"):
+	x = 1
+		
+elif (subject == "Chemistry"):
+	x = 2
+
+elif (subject == "Biology"):
+	x = 3
+
+elif (subject == "Social-Science"):
+	x = 4
+
+else:
+	print("enter valid input")
+
+for i in database:
+	if i == name :
+		y = y.append(database[i(x)]
+		 
+print ("This is {} {} "
+       .format(i, , y))		
+
+
+ 
