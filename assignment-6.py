5de272b73a05b63798ec46d9f9b83ada834aa00b
PavanKumar-98 <63774781+PavanKumar-98@users.noreply.github.com>
2020-04-28 21:12:33 +0530

    Update assignment-6.py

--- assignment-6.py
+++ assignment-6.py
@@ -1,37 +1,49 @@
 database = {}
-database ={"aaa":[20,30,30],"bbb":[33,55,66]}
 Math = 0
 Physics = 1
 Chemistry = 2
 Biology = 3
-Social-Science =4
+Social_Science = 4
 x=0
-name = input("enter the name ")
-subject = input("enter the subject")
-if subject == "math":
+while True:
+	name = input("enter the name \n")
+	subject = input("enter the subject with spaces\n")
+	if name == "":
+		break
+
+	marks = list(subject.split())
+	database[name] = marks
+
+sname = input("enter the name\n")
+ssub = input("enter sub\n ")
+
+if ssub == "Math":
 	x = 0
-	
-elif (subject == "Physics"):
-	x = 1
 		
-elif (subject == "Chemistry"):
+elif (ssub == "Physics"):
+	x = 1
+			
+elif (ssub == "Chemistry"):
 	x = 2
 
-elif (subject == "Biology"):
+elif (ssub == "Biology"):
 	x = 3
 
-elif (subject == "Social-Science"):
+elif (ssub == "Social_Science"):
 	x = 4
 
 else:
 	print("enter valid input")
 
 for i in database:
-	if i == name :
-		y = y.append(database[i(x)]
-		 
-print ("This is {} {} "
-       .format(i, , y))		
+	if i == sname:
+		print(sname,"scored",database[i][x])	
 
+		
+
+
+
+		 
+                                                                               
 
  
