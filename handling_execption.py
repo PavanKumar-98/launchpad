7c9f658cdf9b8700dbef2115b75651f7490f18c8
PavanKumar-98 <63774781+PavanKumar-98@users.noreply.github.com>
2020-05-10 17:52:09 +0530

    Add files via upload

---
+++ handling_execption.py
@@ -0,0 +1,24 @@
+a = int(input("enter numeratotr"))
+b = int(input("enter denominatotr"))
+ 
+def div(x,y):
+	res = float(x/y)
+
+
+try:
+	div(a,b)
+	print(f"answer is{res}")
+
+except divisionbyZero as err :
+	print("division by zero!")
+
+	# raise NameError(divisionbyZero)
+
+
+
+
+
+
+
+
+
