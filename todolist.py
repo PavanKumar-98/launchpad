030807b67595236040a8083ad661fe338547357d
PavanKumar-98 <63774781+PavanKumar-98@users.noreply.github.com>
2020-05-12 23:09:20 +0530

    Update todolist.py

--- todolist.py
+++ todolist.py
@@ -1,34 +1,35 @@
 import pprint
 
-to_do = []
-done = []
 
 class ToDoList:
-	def __init__(self,list_name ,to_do ,done ):
-		self.list_name = list_name
-		self.to_do = to_do
-		self.done = done
+	def __init__(self):
+		self.list_name = ""
+		self.to_do = []
+		self.done = []
 		
 	
-	def add():
+	def add(self):
 		
-		list_name =input("enter the task ")
-		to_do.append(list_name)
+		self.list_name = input("enter the task ")
+		self.to_do.append(self.list_name)
 
 
-	def markdone():
+	def markdone(self):
 		
-		pprint(to_do)
+		print(self.to_do)
 		i = int(input("enter the task number which is done"))
-		done.append(to_do(i))
-		to_do.pop(i)
+		i = i-1
+		j = self.to_do[i]
+		self.done.append(j)
+		self.to_do.pop(i)
+		
 
 
-	def see_tasks():
-		pprint(to_do)
-		pprint(done)
+	def see_tasks(self):
+		print(f"To do list {self.to_do}")
+		print(f"done list{self.done}")
 
-task_list = ToDoList
+task_list = ToDoList()
 
 while True:
 	ch = int(input("What would you like to do\n\
